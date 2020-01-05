594638e2a8abfac7ba197b9cc2af8129a18fd79d
Wladimir Chopite <wchopite@gmail.com>
2020-01-05 13:25:50 -0300

    [provider rates fixes] fix some problems on provider_rates (controller, endpoints, services...)

--- src/api/controllers/provider_rates.controller.js
+++ src/api/controllers/provider_rates.controller.js
@@ -2,7 +2,17 @@ const ProviderRatesController = {};
 
 ProviderRatesController.findAll = ({ProviderRateService}) => {
   return async (req, res) => {
-    const provider_rates = await ProviderRateService.find();
+    let filter = (req.query.provider) ? {provider: req.query.provider} : undefined;
+
+    const provider_rates = await ProviderRateService.find(filter);
+
+    if (provider_rates.length === 0) {
+      return res
+        .response({message: 'rates for specified provider, not found'})
+        .code(404)
+        .type('application/json');
+    }
+
     return res.response(provider_rates).type('application/json');
   };
 };
@@ -14,7 +24,10 @@ ProviderRatesController.findLast = ({ProviderRateService}) => {
     let provider_rate = await ProviderRateService.findOne(filter);
 
     if (provider_rate.length === 0) {
-      provider_rate = {};
+      return res
+        .response({message: 'rate for specified provider, not found'})
+        .code(404)
+        .type('application/json');
     }
 
     return res.response(provider_rate[0]).type('application/json');
@@ -25,12 +38,29 @@ ProviderRatesController.new = ({ProviderRateService}) => {
   return async (req, res) => {
     let data = (req.payload.provider) ? {provider: req.payload.provider} : undefined;
 
-    const newRate = await ProviderRateService.new(data);
-    return res.response(newRate.ops[0]).type('application/json');
+    if (!data) {
+      return res
+        .response({message: 'You must indicate the provider'})
+        .code(400)
+        .type('application/json');
+    }
+
+    try {
+      const newRate = await ProviderRateService.new(data);
+      return res.response(newRate.ops[0]).type('application/json');
+    } catch(err) {
+      if (err.message === 'ProviderNotAllowed') {
+        return res
+          .response({message: 'Provider not allowed'})
+          .code(400)
+          .type('application/json');
+      }
+      return err;
+    }
   };
 };
 
-module.exports = ({ProviderRateService}) => {
+module.exports = ({ProviderRateService, }) => {
   return {
     findAll: ProviderRatesController.findAll({ProviderRateService}),
     findLast: ProviderRatesController.findLast({ProviderRateService}),
